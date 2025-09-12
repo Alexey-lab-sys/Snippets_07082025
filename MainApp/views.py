cc5ff8cd3e042a9b40e901d10a080b3946158e68
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-09-12 21:34:09 +0300

    Done. M5:step 3-7

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,16 +1,13 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 
-
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
-
 def add_snippet_page(request):
     context = {'pagename': 'Добавление нового сниппета'}
     return render(request, 'pages/add_snippet.html', context)
-
 
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов'}
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from MainApp import views
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
-]
+    path('admin/', admin.site.urls),
+    path('', views.index_page, name='home'),
+    path('snippets/add/', views.add_snippet_page, name='snippets-add'),
+    path('snippets/list/', views.snippets_page, name='snippets-list'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
