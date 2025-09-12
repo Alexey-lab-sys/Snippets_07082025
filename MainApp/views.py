55718729caf56c9ac56edfde5691c5031a7b5be9
Alexey Trebunskiy <a.trebounskiy@gmail.com>
2025-09-12 17:17:53 +0300

    Done. M5:step 0-2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index_page(request):
+    context = {'pagename': 'PythonBin'}
+    return render(request, 'pages/index.html', context)
+
+
+def add_snippet_page(request):
+    context = {'pagename': 'Добавление нового сниппета'}
+    return render(request, 'pages/add_snippet.html', context)
+
+
+def snippets_page(request):
+    context = {'pagename': 'Просмотр сниппетов'}
+    return render(request, 'pages/view_snippets.html', context)
